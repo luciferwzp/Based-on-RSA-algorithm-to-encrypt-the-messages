6c336e976ef12b74b39135bbec7e84e7d08ca2aa
wzp <1908488207@qq.com>
2019-08-29 19:16:22 +0800

    RSA

--- RSA.h
+++ RSA.h
@@ -6,13 +6,17 @@
 #include<time.h>
 #include<math.h>
 #include<fstream>
+#include <boost/multiprecision/cpp_int.hpp>
+#include <boost/multiprecision/random.hpp>
+namespace bm = boost::multiprecision;
+
 struct Key
 {
-	//公钥(ekey, pkey): (e,n)
-	long pkey;
-	long ekey;
+	//公钥(ekey, pkey): (e, n)
+	bm::int1024_t pkey;
+	bm::int1024_t ekey;
 	//私钥(dkey, pkey): (d, n)
-	long dkey;
+	bm::int1024_t dkey;
 };
 
 class RSA
@@ -24,25 +28,25 @@ public:
 		return _key;
 	}
 	void ecrept(const char* plain_file_in, const char* ecrept_file_out,
-		long ekey, long pkey);//对文件进行解密
+		bm::int1024_t ekey, bm::int1024_t pkey);//对文件进行解密
 	void decrept(const char* ecrept_file_in, const char* plain_file_out,
-		long dkey, long pkey);//对文件进行加密
+		bm::int1024_t dkey, bm::int1024_t pkey);//对文件进行加密
 
-	std::vector<long> ecrept(std::string& str_in, long ekey, long pkey);//对字符串进行加密
-	std::string decrept(std::vector<long>& ecrept_str, long dkey, long pkey);//对字符串进行解密	
+	std::vector<bm::int1024_t> ecrept(std::string& str_in, bm::int1024_t ekey, bm::int1024_t pkey);//对字符串进行加密
+	std::string decrept(std::vector<bm::int1024_t>& ecrept_str, bm::int1024_t dkey, bm::int1024_t pkey);//对字符串进行解密	
 
-	void printInfo(std::vector<long>& ecrept_str);//打印解密后的信息
+	void printInfo(std::vector<bm::int1024_t>& ecrept_str);//打印解密后的信息
 private:
 	//加密单个信息
-	long ecrept(long msg, long ekey, long pkey);//加密
-	long produce_prime();//产生素数
-	bool is_prime(long prime);//判断是否为素数
+	bm::int1024_t ecrept(bm::int1024_t msg, bm::int1024_t ekey, bm::int1024_t pkey);//加密
+	bm::int1024_t produce_prime();//产生素数
+	bool is_prime(bm::int1024_t prime);//判断是否为素数
 	void produce_keys();//存放公钥秘钥
-	long produce_pkey(long prime1, long prime2);//pq->n
-	long produce_orla(long prime1, long prime2);//(p-1)(q-1)
-	long produce_ekey(long orla);//产生公钥
-	long produce_gcd(long ekey, long orla);//互质
-	long produce_dkey(long ekey, long orla);//产生秘钥
+	bm::int1024_t produce_pkey(bm::int1024_t prime1, bm::int1024_t prime2);//pq->n
+	bm::int1024_t produce_orla(bm::int1024_t prime1, bm::int1024_t prime2);//(p-1)(q-1)
+	bm::int1024_t produce_ekey(bm::int1024_t orla);//产生公钥
+	bm::int1024_t produce_gcd(bm::int1024_t ekey, bm::int1024_t orla);//互质
+	bm::int1024_t produce_dkey(bm::int1024_t ekey, bm::int1024_t orla);//产生秘钥
 
 private:
 	Key _key;
---
+++ test.cpp
@@ -0,0 +1,17 @@
+#include"RSA.h"
+
+int main()
+{
+	RSA rsa;
+	Key key = rsa.getKey();
+	std::string str;
+	std::cout << "请输入需要加密的信息：" << std::endl;
+	getline(std::cin, str);
+	std::vector<long> e_in = rsa.ecrept(str, key.ekey, key.pkey);
+	std::string de_out = rsa.decrept(e_in, key.dkey, key.pkey);
+	std::cout << "加密后的信息：" << std::endl;
+	rsa.printInfo(e_in);
+	std::cout << "解密后的信息：" << std::endl;
+	std::cout << de_out << std::endl;
+	return 0;
+}
